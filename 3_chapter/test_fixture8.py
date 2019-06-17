f410bf258ef9decea9eb7d1c79b63575b77496a4
Yury Shpyntov <Yuriy.Shpyntov@rt.ru>
2019-06-17 14:38:18 +0300

    markers

---
+++ 3_chapter/test_fixture8.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+class TestMainPage1(object):
+
+    @pytest.mark.smoke
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+
+    @pytest.mark.regression
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+# pytest -s -v -m smoke test_fixture8.py
+# pytest -s -v -m "not smoke" test_fixture8.py
+# pytest -s -v -m "smoke or regression" test_fixture8.py
